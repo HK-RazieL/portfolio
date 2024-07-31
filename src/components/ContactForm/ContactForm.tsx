382cd5dd51eb8a44b99aee9b163355336594e73d
HK-RazieL <hkraziel@gmail.com>
2024-07-31 19:10:57 +0300

    Added form and animations for contacts

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,56 +1,118 @@
+"use client";
+import emailjs from "@emailjs/browser";
+import { AnimatePresence, motion } from "framer-motion";
+import Image from "next/image";
+import { FormEvent, useState } from "react";
 import Input from "../Input/Input";
 
 export default function ContactForm() {
+    const [sent, setSent] = useState<boolean>(false);
+
+    const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        try {
+            event.preventDefault();
+            event.stopPropagation();
+
+            const formData = new FormData(event.currentTarget);
+            formData.append("to_name", "RazieL");
+
+            const formDataObj: Record<string, any> = {};
+            formData.forEach((value, key) => {
+                formDataObj[key] = value;
+            });
+
+            emailjs
+                .send(
+                    process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID ?? "",
+                    process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID ?? "",
+                    formDataObj,
+                    process.env.NEXT_PUBLIC_EMAILJS_USER_ID
+                )
+                .then(() => {
+                    setSent(true);
+                });
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
-        <div className="space-y-2 animate-appearFromBottom2">
-            <h2 className="text-h2">Contact me</h2>
-            <p className="text-secondary">
-                You can also contact me through this form
-            </p>
-            <form action="" className="flex flex-col gap-4">
-                <Input
-                    type="text"
-                    label="From:"
-                    id="from"
-                    placeholder="Terry Bogard"
-                />
-                <div className="flex flex-col gap-2">
-                    <label htmlFor="from">From</label>
-                    <input
-                        id="from"
-                        type="text"
-                        placeholder="Terry Bogard"
-                        className="p-2 pt-6 rounded !bg-gray-950"
-                    />
-                </div>
-                <div className="flex flex-col gap-2">
-                    <label htmlFor="email">Email</label>
-                    <input
-                        id="email"
-                        type="email"
-                        placeholder="example@mail.com"
-                        className="p-2 rounded !bg-gray-950"
-                    />
-                </div>
-                <div className="flex flex-col gap-2">
-                    <label htmlFor="subject">Subject</label>
-                    <input
-                        id="subject"
-                        type="text"
-                        placeholder="Hello"
-                        className="p-2 rounded !bg-gray-950"
-                    />
-                </div>
-                <label htmlFor="message">Message</label>
-                <div className="flex flex-col gap-2">
-                    <textarea
-                        rows={10}
-                        placeholder="I would like to hear from you!"
-                        className="p-2 rounded !bg-gray-950 resize-none"
-                    />
-                </div>
-                <button className="bg-gray-900 rounded p-1">Send</button>
-            </form>
+        <div className="animate-appearFromBottom2 pt-10">
+            <AnimatePresence>
+                {!sent ? (
+                    <motion.div
+                        key="form"
+                        initial={{ opacity: 0, y: 250 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        exit={{ opacity: 0, y: -350 }}
+                        transition={{ duration: 0.5 }}
+                        className="space-y-4"
+                    >
+                        <div>
+                            <h2 className="text-h2">Contact me</h2>
+                            <p className="text-secondary">
+                                You can also contact me through this form
+                            </p>
+                        </div>
+                        <form
+                            onSubmit={onSubmit}
+                            className="flex flex-col gap-4"
+                        >
+                            <Input
+                                name="from_name"
+                                type="text"
+                                label="From:"
+                                id="from"
+                                placeholder="Terry Bogard"
+                            />
+                            <Input
+                                name="email"
+                                type="email"
+                                label="Email:"
+                                id="email"
+                                placeholder="example@example.com"
+                            />
+                            <label htmlFor="message">Message:</label>
+                            <div className="flex flex-col gap-2">
+                                <textarea
+                                    name="message"
+                                    id="message"
+                                    rows={10}
+                                    placeholder="I would like to hear from you!"
+                                    className="p-2 !bg-transparent border border-secondary resize-none outline-none"
+                                />
+                            </div>
+                            <button
+                                disabled={sent}
+                                type="submit"
+                                className="bg-gray-900 rounded p-1 hover:outline outline-[1px] outline-secondary"
+                            >
+                                Send
+                                {sent && (
+                                    <Image
+                                        src="/icons/nodejs-icon.png"
+                                        alt="icon"
+                                        width="10"
+                                        height="10"
+                                    />
+                                )}
+                            </button>
+                        </form>
+                    </motion.div>
+                ) : (
+                    <motion.div
+                        key="thankyou"
+                        initial={{ opacity: 0, y: 250 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        exit={{ opacity: 0, y: -550 }}
+                        transition={{ duration: 0.5, delay: 0.5 }}
+                    >
+                        <p className="h-[300px] flex items-center justify-center">
+                            Thank you for contacting me!
+                        </p>
+                    </motion.div>
+                )}
+            </AnimatePresence>
         </div>
     );
 }
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,5 +1,9 @@
+"use client";
+import { useState } from "react";
+
 export type InputType = {
     id?: string;
+    name: string;
     type: string;
     placeholder?: string;
     className?: string;
@@ -10,29 +14,34 @@ export type InputType = {
 
 export default function Input({
     id,
+    name,
     type,
     className,
     labelClassName,
     label,
-    defaultValue,
+    defaultValue = "",
     placeholder,
 }: InputType) {
+    const [value, setValue] = useState<any>(defaultValue);
+
     return (
         <div className="relative flex w-full group transition-all ease-in-out py-2">
             {label && label !== "" ? (
                 <label
                     htmlFor={id ?? ""}
-                    className={`absolute top-[1rem] transition-all duration-300 group-focus-within:-translate-y-[1.5rem] ${labelClassName}`}
+                    className={`absolute top-[1rem] transition-all duration-300 group-focus-within:-translate-y-[1.5rem] ${value !== "" ? "-translate-y-[1.5rem]" : ""} ${labelClassName}`}
                 >
                     {label}
                 </label>
             ) : null}
             <input
+                name={name}
                 defaultValue={defaultValue}
                 id={id}
                 type={type}
                 placeholder={placeholder}
                 className={`p-2 !bg-transparent w-full placeholder:text-transparent focus:placeholder:text-secondary outline-none border-b border-secondary focus:border-b focus:border-secondary focus:border-t-0 ${className}`}
+                onChange={(e) => setValue(e.target.value)}
             ></input>
         </div>
     );
